85c5e29644d712a0337ffd4789c1adf510ea2f3c
valex <valerapanow03@gmail.com>
2023-03-28 22:26:40 +0300

    updade url

--- aiogram_bot/api.py
+++ aiogram_bot/api.py
@@ -4,7 +4,7 @@ from aiogram import types
 
 async def create_user(data: dict, message: types.Message):
     async with aiohttp.ClientSession() as session:
-        async with session.post('https://django-backend-production.up.railway.app//sign-up', json=data) as resp:
+        async with session.post('https://django-backend-production.up.railway.app/sign-up', json=data) as resp:
             response = await resp.json()
             if resp.status == 400:
                 msg = response.get('message')
