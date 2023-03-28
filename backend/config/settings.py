8dece098ed7ba66410dc3bd81e55bdfcf6dce593
valex <valerapanow03@gmail.com>
2023-03-28 22:37:22 +0300

    update allowed_hosts and csrf_trusted

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,7 +10,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-backend-production.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = ['django-backend-production.up.railway.app']
+
 
 
 # Application definition
