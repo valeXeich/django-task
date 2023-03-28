393dae66a1084d2e646ad3bb7689d8804e7638f0
valex <valerapanow03@gmail.com>
2023-03-28 21:45:27 +0300

    test

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(os.getenv('POSTGRES_HOST'), 'host')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
